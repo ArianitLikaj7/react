f951057129c77128024b2fa5cf58e37683099c3e
arianit <arianitlikaj46@gmail.com>
2024-01-31 15:15:33 +0100

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';   
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function Greeting(){
+//     return <div>
+//              <h1>
+//                  Hello World, Welcome in the world of React
+//               </h1>
+//         </div>;
+//     return  
+ 
+ //}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const Greeting = ()=> {
+//     return React.createElement('div',{},React.createElement('h1',{},"Hellooo"));
+// }
+
+import "./index.css"
+function BookList() {
+    return (
+       <section className='booklist'>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+        
+        </section>
+    );  
+}
+
+const Book = () => {
+    return (
+        <article className='book'>
+            <Image></Image>
+            <Title />
+            <Author />
+        </article>
+   
+    )
+};
+
+const Image = ()=> <img src="https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL300_SR300,200_.jpg" alt="" />;
+const Title = ()=> <h1>I love you to the Moon and Back</h1>;
+const Author = ()=> <h4>Amelia Hepworth</h4>;
+ReactDom.render(<BookList/>, document.getElementById('root'));
+
